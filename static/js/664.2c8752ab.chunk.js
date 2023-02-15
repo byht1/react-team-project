1bbe5cd9ceadefef4eccc0d9ae4d31e677242f30
KautTanya <KautTanya@users.noreply.github.com>
2023-02-15 21:20:00 +0000

    Deploying to gh-pages from @ byht1/react-team-project@b8b63918352e42dccfa2f5505dbddd4d2e07295d 🚀

--- static/js/664.2c8752ab.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_team_project=self.webpackChunkreact_team_project||[]).push([[664],{664:function(e,t,c){c.r(t);c(791);var n=c(329);t.default=function(){return(0,n.jsx)("div",{children:"NewsPage Component "})}}}]);
-//# sourceMappingURL=664.2c8752ab.chunk.js.map
