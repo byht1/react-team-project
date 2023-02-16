738a3b6ea128c03529fb77582a779127b3ad3a50
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-16 19:30:16 +0200

    fix

--- src/api/basic.js
+++ src/api/basic.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // https://node-team-project.onrender.com/docs Документаці як в 8 дз по React
 // Первий запит може йти довго так як server безкоштовний і він "засинає"
 
-const URL = 'https://node-team-project.onrender.com';
+const URL = 'https://node-team-project.onrender.com/api';
 
 const server = axios.create({
   withCredentials: true,
