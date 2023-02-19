e14b4d6cc9cdd7361d507f853221f4128e68ac5b
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-02-19 11:58:28 +0200

    homep

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
-import { Text } from 'components/global/text';
-
+import { useNavigate } from 'react-router-dom';
+import { Background, Box, Image, MaxBox, HomeTitle } from './Home.styled';
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <Text>Home Component</Text>
-    </div>
+    // <Background>
+    <MaxBox>
+      <Background>
+        <Box>
+          <HomeTitle>Take good care of your small pets</HomeTitle>
+          <button type="button" onClick={() => navigate('/addpet/step1')}>
+            Button
+          </button>
+          {/* <Image src={require('../../img/girl_mobile.png')}></Image> */}
+        </Box>
+      </Background>
+    </MaxBox>
+    // </Background>
   );
 };
 
