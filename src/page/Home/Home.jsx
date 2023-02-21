ef3f263b387a10e134942bc15f873442620fd90f
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-02-21 20:44:24 +0200

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from '../../components/global/Container/Container';
+// import { Container } from '../../components/global/Container/Container';
 import {
   Background,
   Box,
