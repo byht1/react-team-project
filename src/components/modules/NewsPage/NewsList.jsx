269c0964ad94b255035cbf83aef9be7b59a6d7d8
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-21 18:48:23 +0200

    fix

--- src/components/modules/NewsPage/NewsList.jsx
+++ src/components/modules/NewsPage/NewsList.jsx
@@ -1,18 +1,10 @@
-
-import { NewsItem } from "./NewsItem";
+import { NewsItem } from './NewsItem';
 import { List } from './NewsPage.styled';
 export const NewsList = ({ data }) => {
   return (
     <List>
       {data.map(({ title, text, day, link }) => {
-        return (
-          <NewsItem
-            title={title}
-            text={text}
-            day={day}
-            link={link}
-          />
-        );
+        return <NewsItem key={title + link} title={title} text={text} day={day} link={link} />;
       })}{' '}
     </List>
   );
