54301aed50fbfc9d826a817bd505412a40e6ada4
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-22 23:34:00 +0200

    fix

--- src/api/basic.js
+++ src/api/basic.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // https://node-team-project.onrender.com/docs Документаці як в 8 дз по React
 // Первий запит може йти довго так як server безкоштовний і він "засинає"
 
-const URL = 'http://localhost:5000/api';
-// const URL = 'https://node-team-project.onrender.com/api';
+// const URL = 'http://localhost:5000/api';
+const URL = 'https://node-team-project.onrender.com/api';
 
 const server = axios.create({
   baseURL: URL,
@@ -24,6 +24,9 @@ server.interceptors.response.use(
   async error => {
     if (error.response.status === 403 && !error.config._isRetry) {
       const refreshToken = localStorage.getItem('refreshToken');
+
+      if (!refreshToken) return Promise.reject(error);
+
       try {
         const { data } = await server.post('/auth/refresh', { refresh_token: refreshToken });
 
