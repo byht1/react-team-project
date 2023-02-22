e36e2ab9c9200985dff766cfcfd2b376e8b84018
Vadim luginets <pebeeo@gmail.com>
2023-02-22 20:20:34 +0200

    User-page
    Now user page render using data from backend

--- src/components/modules/User/UserInfo/UserInfo.jsx
+++ src/components/modules/User/UserInfo/UserInfo.jsx
@@ -1,68 +1,6 @@
 import React, { useState } from 'react';
-import {
-  Form,
-  Item,
-  Input,
-  SpanLogout,
-  Cover,
-  FildName,
-  Icon,
-  EditIc,
-  LogoutIc,
-  CheckIc,
-  EditIcBlack,
-} from './UserInfo.styled';
-
-export function UserInputInfo({
-  fildName,
-  whatInputIsEditing,
-  iconOnClick,
-  whichIconToShow,
-  setWhichIconToShow,
-}) {
-  const [value, setValue] = useState('Loasd oasdsa');
-  const onChange = e => {
-    console.log(fildName, value);
-    setValue(e.target.value);
-  };
-  return (
-    <Item>
-      <FildName>{`${fildName}:`}</FildName>
-      <Item>
-        <Input
-          id={fildName}
-          type="text"
-          disabled={whatInputIsEditing === fildName ? false : true}
-          value={value}
-          onChange={onChange}
-        />
-      </Item>
-      <Icon href={`#${fildName}`}>
-        {whatInputIsEditing === fildName ? (
-          <CheckIc
-            onClick={() => {
-              setWhichIconToShow('orange');
-              iconOnClick('qwe');
-            }}
-          />
-        ) : (
-          <>
-            {whichIconToShow === 'black' ? (
-              <EditIcBlack />
-            ) : (
-              <EditIc
-                onClick={() => {
-                  iconOnClick(fildName);
-                  setWhichIconToShow('black');
-                }}
-              />
-            )}
-          </>
-        )}
-      </Icon>
-    </Item>
-  );
-}
+import { UserInputInfo } from '../UserInputInfo';
+import { Form, SpanLogout, Cover, LogoutIc } from './UserInfo.styled';
 
 export const UserInfo = () => {
   const [whatInputIsEditing, setWhatInputIsEditing] = useState('');
@@ -73,35 +11,35 @@ export const UserInfo = () => {
         <UserInputInfo
           fildName={'Name'}
           whatInputIsEditing={whatInputIsEditing}
-          iconOnClick={setWhatInputIsEditing}
+          setWhatInputIsEditing={setWhatInputIsEditing}
           whichIconToShow={whichIconToShow}
           setWhichIconToShow={setWhichIconToShow}
         />
         <UserInputInfo
           fildName={'Email'}
           whatInputIsEditing={whatInputIsEditing}
-          iconOnClick={setWhatInputIsEditing}
+          setWhatInputIsEditing={setWhatInputIsEditing}
           whichIconToShow={whichIconToShow}
           setWhichIconToShow={setWhichIconToShow}
         />
         <UserInputInfo
           fildName={'Birthday'}
           whatInputIsEditing={whatInputIsEditing}
-          iconOnClick={setWhatInputIsEditing}
+          setWhatInputIsEditing={setWhatInputIsEditing}
           whichIconToShow={whichIconToShow}
           setWhichIconToShow={setWhichIconToShow}
         />
         <UserInputInfo
           fildName={'Phone'}
           whatInputIsEditing={whatInputIsEditing}
-          iconOnClick={setWhatInputIsEditing}
+          setWhatInputIsEditing={setWhatInputIsEditing}
           whichIconToShow={whichIconToShow}
           setWhichIconToShow={setWhichIconToShow}
         />
         <UserInputInfo
           fildName={'City'}
           whatInputIsEditing={whatInputIsEditing}
-          iconOnClick={setWhatInputIsEditing}
+          setWhatInputIsEditing={setWhatInputIsEditing}
           whichIconToShow={whichIconToShow}
           setWhichIconToShow={setWhichIconToShow}
         />
--- src/redux/index.js
+++ src/redux/index.js
@@ -11,6 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authSlice } from './auth';
+import { userSliceReducer } from './user';
 
 const middleware = [
   ...getDefaultMiddleware({
@@ -29,6 +30,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authSlice.reducer),
+    user: userSliceReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
