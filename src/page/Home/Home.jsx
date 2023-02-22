b87ddfdffe4dcc08c1b68e13ee0c2acf6cdcda26
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-02-22 21:07:28 +0200

    fixes

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -11,21 +11,16 @@ import {
 } from './Home.styled';
 const Home = () => {
   return (
-    // <Background>
     <MaxBox>
       <Background>
         <Box>
           <HomeTitle>Take good care of your small pets</HomeTitle>
           <MobileImage src={require('../../img/home/girl_mobile.png')} />
           <TabletImage src={require('../../img/home/tablet_girl.png')} />
-          {/* <Container>
-            <HomeTitle>Take good care of your small pets</HomeTitle> */}
           <DesktopImage src={require('../../img/home/girl_desk.png')} />
-          {/* </Container> */}
         </Box>
       </Background>
     </MaxBox>
-    // </Background>
   );
 };
 
