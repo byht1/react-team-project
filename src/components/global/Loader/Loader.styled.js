7c656b81fd040e8c2b38fcff01aaa3fd44125e47
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-23 16:44:44 +0200

    fix

--- src/components/global/Loader/Loader.styled.js
+++ src/components/global/Loader/Loader.styled.js
@@ -2,21 +2,21 @@ import styled from 'styled-components';
 import { ThreeCircles } from 'react-loader-spinner';
 
 export const NewLoader = styled(ThreeCircles)`
-        position: relative;
-        top: 50%;
-        left: 50%;
-        height="100"
-        width="100"
-        margin-top: -50px;
-        margin-left: -50px;
-        color: red;
-`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  height: 100px;
+  width: 100px;
+  margin-top: -50px;
+  margin-left: -50px;
+  color: red;
+`;
 
 export const Block = styled.div`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh;
-        background: #FDF7F2;
-       
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: ${p => `rgba(253, 247, 242, ${p.opacity.opacity})`};
+`;
