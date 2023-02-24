38b42d788e6424f12fb9ca7701682cb2bec2a238
zlatan-dp <zlatandnepr@gmail.com>
2023-02-24 21:55:30 +0200

    fixed yap

--- src/components/modules/LoginForm/LoginSchema.js
+++ src/components/modules/LoginForm/LoginSchema.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const loginSchema = yup.object({
   email: yup
     .string()
-    .min(7, 'Must be more than 7 digits')
+    .min(7, 'Must be more than 7 characters')
     .max(63, 'Must be less than 63 characters')
     .email('minimum 2 characters before @, cannot start with a dash, only Latin letters')
     .required('The field is mandatory')
