a72c02e1e1715b5e9e5270eee2523472ff8ecb8b
Vitalik Dykhal <vitalikbyht1@gmail.com>
2023-02-25 19:04:56 +0200

    fix

--- src/components/modules/NewsPage/NewsList.jsx
+++ src/components/modules/NewsPage/NewsList.jsx
@@ -4,8 +4,7 @@ import { useState } from 'react';
 import { NewsItem } from './NewsItem';
 import { Box } from 'components/global/Box';
 
-import {InputSearch} from '../../global/InputSearch';
-// import { Filter } from './Filter';
+import { InputSearch } from '../../global/InputSearch';
 import { List, TitleNews } from './NewsPage.styled';
 
 export const NewsList = () => {
@@ -16,35 +15,28 @@ export const NewsList = () => {
     queryKey: ['news'],
     onSuccess: date => {
       setData(date);
-    }
+    },
   });
- 
-  const changeFilter = event => {
-    setFilter(event.currentTarget.value);
-  };
 
   const normalizeFilter = filter.toLowerCase();
   const visibleNews = data.filter(todo => todo.title.toLowerCase().includes(normalizeFilter));
 
-  const handleClear = () => {
-    setFilter('');
-  };
-
   return (
     <Box display="flex" flexDirection="column" justifyContent="center">
       <TitleNews textAlign="center">News</TitleNews>
-      <InputSearch onChange={changeFilter} value={filter} onClick={handleClear}/>
+      <InputSearch change={setFilter} value={filter} />
       <List>
         {isSuccess &&
-        visibleNews.sort(function (a, b) {
-            return new Date(b.date) - new Date(a.date);
-          })
-          .slice(0, 6)
-          .map(({ title, description, date, url, _id }) => {
-            return (
-              <NewsItem key={_id} title={title} description={description} date={date} url={url} />
-            );
-          })}{' '}
+          visibleNews
+            .sort(function (a, b) {
+              return new Date(b.date) - new Date(a.date);
+            })
+            .slice(0, 6)
+            .map(({ title, description, date, url, _id }) => {
+              return (
+                <NewsItem key={_id} title={title} description={description} date={date} url={url} />
+              );
+            })}{' '}
       </List>
     </Box>
   );
