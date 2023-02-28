068f212203822e27214643f2b9ca5e63c51e8757
ViktoriaZinyak <100145684+ViktoriaZinyak@users.noreply.github.com>
2023-02-28 12:46:27 +0200

    fixed date

--- src/components/modules/FormAddNotice/helpers/dateConverter.jsx
+++ src/components/modules/FormAddNotice/helpers/dateConverter.jsx
@@ -1,7 +1,10 @@
 export const dateConverter = date => {
-  const yyyy = date.getFullYear();
-  let mm = date.getMonth() + 1; // Months start at 0!
-  let dd = date.getDate();
+  // console.log(date);
+  const dateObject = new Date(date);
+
+  const yyyy = dateObject.getFullYear();
+  let mm = dateObject.getMonth() + 1; // Months start at 0!
+  let dd = dateObject.getDate();
 
   if (dd < 10) dd = '0' + dd;
   if (mm < 10) mm = '0' + mm;
