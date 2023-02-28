fa24eea6b47cb1573bd649685898094b527765a4
Inna Lazorko <inna-h@i.ua>
2023-02-28 02:02:12 +0200

    fix regular expression

--- src/components/modules/ModalAddsPet/validationSchema.js
+++ src/components/modules/ModalAddsPet/validationSchema.js
@@ -3,7 +3,10 @@ import * as yup from 'yup';
 export const schemaAddMyPetForm = yup.object({
   petName: yup
     .string()
-    .matches(/^[a-zA-ZА-Яа-яЁёЇїІіЄєҐґ' ]+$/u, 'Name can contain only letters and spaces.')
+    .matches(
+      /^[A-Za-zА-Яа-яЁёҐґІіЇїЄє\s,'"'-.]+(?:\s+[A-Za-zА-Яа-яЁёҐґІіЇїЄє]+){0,3}$/u,
+      'Name can contain only letters and spaces.'
+    )
     .min(2, 'The name must contain more than 2 characters')
     .max(20, 'Maximum name length 20 characters')
     .required('This field is required!'),
@@ -37,7 +40,10 @@ export const schemaAddMyPetForm = yup.object({
     }),
   petBreed: yup
     .string()
-    .matches(/^[a-zA-ZА-Яа-яЁёЇїІіЄєҐґ' ]+$/u, 'Breed can contain only letters and spaces.')
+    .matches(
+      /^[A-Za-zА-Яа-яЁёҐґІіЇїЄє\s,'"'-.]+(?:\s+[A-Za-zА-Яа-яЁёҐґІіЇїЄє]+){0,3}$/u,
+      'Breed can contain only letters and spaces.'
+    )
     .min(2, 'Breed must contain more than 2 characters')
     .max(50, 'Maximum breed length 50 characters')
     .required('This field is required!'),
